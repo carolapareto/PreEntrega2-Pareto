7cb378bfe3d200a996516f8dc3d1a20e0f9fb43a
Carola Pareto <paretocarola@gmail.com>
2023-03-28 22:30:36 -0300

    JSON, Storage // Alerts y prompts reemplazados por uso de DOM y eventos

--- js/script.js
+++ js/script.js
@@ -2,6 +2,19 @@ let total = 0;
 let productos = [];
 let mensaje = "";
 
+// Elementos del DOM que se van a utilizar
+const carritoElement = document.getElementById("carrito");
+const totalElement = document.getElementById("total");
+
+
+if (localStorage.getItem("carrito")) {
+  const carritoGuardado = JSON.parse(localStorage.getItem("carrito"));
+  productos = carritoGuardado.productos;
+  total = carritoGuardado.total;
+  carritoElement.innerHTML = carritoGuardado.html;
+  totalElement.innerText = "Total: $" + total.toFixed(2);
+}
+
 class Producto {
   constructor(nombre, precio) {
     this.nombre = nombre;
@@ -10,101 +23,94 @@ class Producto {
   }
 }
 
+// Agregar productos al array productos
 productos.push(new Producto("Mat", 3500));
 productos.push(new Producto("Bolster", 4000));
 productos.push(new Producto("Zafu", 4500));
 productos.push(new Producto("Bloque", 1500));
 
 function agregarProducto() {
-  let nombreProducto = prompt("Ingrese el nombre del producto:\n- Mat\n- Bolster\n- Zafu\n- Bloque");
-  let productoEncontrado = productos.find(producto => producto.nombre.toLowerCase() === nombreProducto.toLowerCase());
+  const nombreElement = document.getElementById("nombre");
+  const cantidadElement = document.getElementById("cantidad");
+  const nombreProducto = nombreElement.value;
+
+  // Buscar el producto en el array productos
+  const productoEncontrado = productos.find(
+    producto =>
+    producto.nombre.toLowerCase() === nombreProducto.toLowerCase()
+  );
+  
 
   if (productoEncontrado) {
-    let cantidadProducto = parseInt(prompt("Ingrese la cantidad de productos que desea agregar: "));
+    const cantidadProducto = parseInt(cantidadElement.value);
     productoEncontrado.cantidad += cantidadProducto;
     total += productoEncontrado.precio * cantidadProducto;
     mensaje = cantidadProducto + " " + nombreProducto + "(s) se agregó al carrito.";
-    alert(mensaje);
+    // Agregar mensaje al carrito
+    const li = document.createElement("li");
+    li.innerText = mensaje;
+    carritoElement.appendChild(li);
+
+    // Actualizar total del carrito
+    totalElement.innerText = "Total: $" + total.toFixed(2);
+
+    // Ocultar mensaje de carrito vacío si se agregan productos al carrito
+    document.getElementById("mensaje").innerText = "";
   } else {
     mensaje = "Producto inválido.";
-    alert(mensaje);
+    document.getElementById("mensaje").innerText = mensaje;
   }
+
+
+// Guardar el carrito en el local storage
+const carrito = {
+  productos: productos,
+  total: total,
+  html: carritoElement.innerHTML,
+};
+localStorage.setItem("carrito", JSON.stringify(carrito));
+
+// Verificar si el carrito estaba vacío antes de agregar un producto
+if(carritoElement.innerHTML === "") {
+  // Si estaba vacío, eliminar el mensaje de carrito vacío
+  document.getElementById("mensaje").innerText = "";
 }
+
+}
+
+// Escuchar el evento click del botón agregar al carrito
+document.getElementById("agregar").addEventListener("click", agregarProducto);
 
 
 function verCarrito() {
-  if (productos.every(producto => producto.cantidad === 0)) {
+  if (carritoElement.innerHTML === "") {
     mensaje = "El carrito está vacío.";
+    document.getElementById("mensaje").innerText = mensaje;
   } else {
-    let productosTexto = productos.filter(producto => producto.cantidad > 0).map(producto => producto.nombre + " - Precio: $" + producto.precio.toFixed(2) + "- Cantidad " + producto.cantidad).join("\n");
-    mensaje = "Productos en el carrito:\n\n" + productosTexto + "\nTotal: $" + total.toFixed(2);
+    mensaje = "Productos en el carrito:";
+    productos.forEach(producto => {
+      if (producto.cantidad > 0) {
+        mensaje += "\n- " + producto.nombre + ": " + producto.cantidad + " unidad(es) - Precio: $" + producto.precio.toFixed(2);
+      }
+    });
+    mensaje += "\n\nTotal: $" + total.toFixed (2);
+    document.getElementById("mensaje").innerText = mensaje;
   }
-    alert(mensaje);
 }
 
-function buscarProducto() {
-  let nombreProducto = prompt("Ingrese el nombre del producto a buscar:");
-  let productoEncontrado = productos.find(producto => producto.nombre.toLowerCase() === nombreProducto.toLowerCase());
-
-  if (productoEncontrado) {
-    mensaje = "Producto encontrado:\n\n" + productoEncontrado.nombre + " - Precio: $" + productoEncontrado.precio.toFixed(2);
-  } else {
-    mensaje = "Producto no encontrado.";
-  }
-  alert(mensaje);
-}
-
-function filtrarProductos() {
-  let precioMinimo = parseInt(prompt("Ingrese el precio mínimo:"));
-  let productosFiltrados = productos.filter(producto => producto.precio >= precioMinimo);
-
-  if (productosFiltrados.length > 0) {
-    let productosFiltradosTexto = productosFiltrados.map(producto => producto.nombre + " - Precio: $" + producto.precio.toFixed(2)).join("\n");
-    mensaje = "Productos filtrados: \n\n" + productosFiltradosTexto;
-  } else {
-    mensaje = "No se encontraron productos con el precio mínimo especificado.";
-  }
-  alert(mensaje);
-}
+document.getElementById("ver").addEventListener("click", verCarrito);
 
 function vaciarCarrito() {
-  productos.forEach(producto => {
-    producto.cantidad = 0;
-  });
   total = 0;
-  mensaje = "El carrito ha sido vaciado.";
-  alert(mensaje);
+  productos.forEach(producto => (producto.cantidad = 0));
+  carritoElement.innerHTML = "";
+  totalElement.innerText = "Total: $" + total.toFixed(2);
+  mensaje = "El carrito está vacío.";
+  document.getElementById("mensaje").innerText = mensaje;
+  
+  // Borrar el carrito del local storage
+  localStorage.removeItem("carrito");
 }
 
-let continuar = true;
-
-while (continuar) {
-  let opcion = prompt("Bienvenido/a a la tienda de productos\nSeleccione una opción:\n1. Agregar un producto\n2. Ver el carrito \n3. Buscar producto\n4. Filtrar productos por precio mínimo\n5. Vaciar carrito\n6. Salir");
-
-  switch (opcion) {
-    case "1":
-      agregarProducto();
-      break;
-    case "2":
-      verCarrito();
-      break;
-    case "3":
-      buscarProducto();
-      break;
-    case "4":
-      filtrarProductos();
-      break;
-    case "5":
-      vaciarCarrito();
-      continuar = false;
-      break;
-      case "6":
-      continuar = false;
-      break;
-    default:
-      mensaje = "Opción inválida.";
-      alert(mensaje);
-      break;
-  }
-}
+document.getElementById("vaciar").addEventListener("click", vaciarCarrito);
 
